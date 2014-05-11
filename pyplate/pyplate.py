ce054462317a52d9eb3d8d61a640bca73854ee62
ThatITNinja <ThatITNinja@hotmail.com>
2014-05-11 04:56:41 -0400

    fix inheritance and more major work

--- pyplate/pyplate.py
+++ pyplate/pyplate.py
@@ -27,20 +27,29 @@ _DOUBLE, _DOUBLE_SIZE  = "d", 8
 
 
 class template(object):
-	def __init__(self, f_name, name=""):
-		self.f_name = f_name
+	def __init__(self, name=""):
 		self.name = name
 		self.data_objects = []
+		self.extracted = {}
 	def __call__(self, *args, **kwargs):
 		for obj in args:
 			self.data_objects.append(obj)
+		return self
+	def append(self, obj):
+		self.data_objects.append(obj)
 	def cast(self, f_obj):
 		file_length = os.path.getsize(f_obj.name)
 		for obj in self.data_objects:
 			obj.cast(f_obj, file_length)
+	def extract(self, f_obj):
+		for obj in self.data_objects:
+			extracted_data = obj.extract(f_obj)
+			if extracted_data != None:
+				self.extracted[obj.name] = extracted_data[1]
+		return self.extracted
 
 class BaseDatatype(object):
-	def __init__(self, name="", repeat=1, cast_start_offset = None, endianess=NATIVE_ENDIAN):
+	def __init__(self, name="", repeat=1, endianess=NATIVE_ENDIAN):
 		#file IO related
 		self.f_offset_start = []
 		self.length = 0
@@ -53,91 +62,106 @@ class BaseDatatype(object):
 	def cast(self, f_obj, file_length):
 		for cast_num in range(self.repeat):
 			self.f_offset_start.append(f_obj.tell())
-			if (f_obj.tell() + self.length) > file_length:
+			if (f_obj.tell() + self.length - 1) > file_length:
 				raise TypeError("Unable to cast data type to file object at offset: %s" % str(f_obj.tell()))
 			#push the pointer forward to account for it being "read"
 			f_obj.read(self.length)
 		self.casted = True
 	def extract(self, f_obj):
+		extracted_values = []
 		if not self.casted:
 			self.cast(f_obj, os.path.getsize(f_obj.name))
 		for start in self.f_offset_start:
 			f_obj.seek(start)
 			type_data = f_obj.read(self.length)
-			yield struct.unpack(self.endianess + self.unpack_sequence, type_data)
+			extracted_values.append(struct.unpack(self.endianess + self.unpack_sequence, type_data))
+		return extracted_values
+
+#Utility "types", it's just a simple way to jump around the file
+class FSeek(object):
+	def __init__(self, offset):
+		self.offset = offset
+	def cast(self, f_obj, *args, **kwargs):
+		f_obj.seek(self.offset)
+	def extract(self):
+		"""
+		Simple compliance for the extraction of objects
+		"""
+		for x in range(0):
+			yield None
 
 class BYTE(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(BYTE, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _BYTE_SIZE
 		self.unpack_sequence = _BYTE * self.repeat
 class CHAR(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(UINT, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _CHAR_SIZE
 		self.unpack_sequence = _CHAR * self.repeat
 class SCHAR(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(SCHAR, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _SCHAR_SIZE
 		self.unpack_sequence = _SCHAR * self.repeat
 class UCHAR(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(UCHAR, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _UCHAR_SIZE
 		self.unpack_sequence = _UCHAR * self.repeat
 class BOOL(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(BOOL, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _BOOL_SIZE
 		self.unpack_sequence = _BOOL * self.repeat
 class SHORT(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(SHORT, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _SHORT_SIZE
 		self.unpack_sequence = _SHORT * self.repeat
 class USHORT(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(USHORT, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _USHORT_SIZE
 		self.unpack_sequence = _USHORT * self.repeat
 class INT(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(INT, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _INT_SIZE
 		self.unpack_sequence = _INT * self.repeat
 class UINT(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(UINT, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _UINT_SIZE
 		self.unpack_sequence = _UINT * self.repeat
 class LONG(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(LONG, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _LONG_SIZE
 		self.unpack_sequence = _LONG * self.repeat
 class ULONG(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(ULONG, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _ULONG_SIZE
 		self.unpack_sequence = _ULONG * self.repeat
 class LLONG(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(LLONG, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _LLONG_SIZE
 		self.unpack_sequence = _LLONG * self.repeat
 class ULLONG(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(ULLONG, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _ULLONG_SIZE
 		self.unpack_sequence = _ULLONG * self.repeat
 class FLOAT(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(FLOAT, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _FLOAT_SIZE
 		self.unpack_sequence = _FLOAT * self.repeat
 class DOUBLE(BaseDatatype):
 	def __init__(self, *args, **kwargs):
-		super(DOUBLE, self).__init__(*args, **kwargs)
+		BaseDatatype.__init__(self, *args, **kwargs)
 		self.length = _DOUBLE_SIZE
 		self.unpack_sequence = _DOUBLE * self.repeat
