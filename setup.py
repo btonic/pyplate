53ee193f1891bc49e690bdb9b7c82559be199cad
ThatITNinja <ThatITNinja@hotmail.com>
2014-05-11 05:34:55 -0400

    update setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='Fuzzer',
+      version='0.0.1',
+      description='A binary templating framework written in pure python.',
+      author='ThatITNinja',
+      url='https://github.com/ThatITNinja/pyplate',
+      packages=['pyplate'],
+      test_suite='test'
+     )
